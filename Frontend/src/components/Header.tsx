d66ea7b99dfbf224a18b3084b943d9b22f44f8ad
ajaypalamkunnel <ajaypalamkunnel45@gmail.com>
2025-06-14 12:31:08 +0530

    updated

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -43,17 +43,7 @@ const Header = () => {
             </a>
           </div>
 
-          {/* Search Bar */}
-          <div className="flex-1 max-w-lg mx-8">
-            <div className="relative">
-              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
-              <input
-                type="text"
-                placeholder="Search articles..."
-                className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              />
-            </div>
-          </div>
+        
 
           {/* User Profile */}
           <div className="relative">
